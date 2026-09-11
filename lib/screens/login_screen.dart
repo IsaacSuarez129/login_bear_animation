02ed1ee567b12fbc01e192aeda832150ddf4b34d
IsaacSuarez <158487522+IsaacSuarez129@users.noreply.github.com>
2026-09-10 19:31:40 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,6 +9,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //control para mostrar o ocultar la contraseña
+  bool _obscure = true;
+
   @override
   Widget build(BuildContext context) {
     //Para obtener el tamaño de la pantalla
@@ -23,6 +26,45 @@ class _LoginScreenState extends State<LoginScreen> {
                 width: size.width,
                 height: 200,
                 child: RiveAnimation.asset('login-bear.riv'),
+              ),
+              //para separar espacios entre los elementos
+              SizedBox(height: 10),
+              TextField(
+                //para mostrar el teclado de email
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    prefixIcon: const Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12)
+                    )
+                )
+              ),
+
+              SizedBox(height: 10),
+              // Contrasena
+              TextField(
+                obscureText: _obscure,
+                //para mostrar el teclado de email
+                decoration: InputDecoration(
+                    hintText: 'Contraseña',
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      //If, operador ternario
+                      icon: Icon(
+                        _obscure ? Icons.visibility : Icons.visibility_off
+                      ),
+                      onPressed: () {
+                        //Refresca el icono de visibilidad de la contraseña
+                        setState(() {
+                          _obscure = !_obscure;
+                        });
+                      },
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12)
+                    )
+                )
               ),
             ],
           ),
